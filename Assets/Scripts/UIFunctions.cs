34d46e9ee142e220e59a41308e7b09312c8b14d2
DaemonUmbra <daemon.umbra@gmail.com>
2017-10-07 17:45:23 -0400

    Dumping GateRay to Github

---
+++ Assets/Scripts/UIFunctions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIFunctions : MonoBehaviour {
+    private GameController Controller;
+
+    // Use this for initialization
+    void Start () {
+        Controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Called when Resume Game is clicked in the pause menu
+    /// </summary>
+    public void ResumeGame()
+    {
+        Controller.ResumeGame();
+    }
+
+    /// <summary>
+    /// Loads the Main Menu
+    /// </summary>
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    /// <summary>
+    /// Quits the game, here to make Application.Quit callable from Canvas buttons
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# GateRay
+
+A light redirection game made by members of the WANOBINS team.
